f2f930fad62ba1ae86841577648de229ea3bbeb8
rofiah <adaweach@gmail.com>
2021-08-27 05:40:09 +0700

    delete unused code

--- App.js
+++ App.js
@@ -1,22 +1,16 @@
 import React, {Component} from 'react';
-import { StatusBar, View,Text } from 'react-native';
-import { Provider as PaperProvider } from 'react-native-paper';
+import { StatusBar} from 'react-native';
 import { Provider as StoreProvider } from 'react-redux';
 import store from './src/store';
-
 import { Colors } from './src/styles/Colors';
 import AppNavigation from './src/navigation/AppNavigation';
 
 class App extends Component {
-
     render() {
-      console.log('halaman app');
         return (
             <StoreProvider store={store}>
-              <PaperProvider>
                 <StatusBar backgroundColor={Colors.primary} barStyle="light-content" />
                 <AppNavigation/>
-              </PaperProvider>
           </StoreProvider>  
         );
     }
